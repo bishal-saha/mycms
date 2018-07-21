9fdef0c4ad720e8d58263691a1f83cfa6a6e8860
Bishal Saha <bishal.saha@gmail.com>
2018-07-21 23:29:42 +0530

    Resend Password Email configuration and checkAdmin Middleware integrated

---
+++ mycms-laravel/app/Http/Middleware/checkAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class checkAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check() && Auth::user()->is_admin) {
+            return $next($request);
+        }
+
+        return redirect()->route('login')->with('warning', 'You are not authorized to visit this page.');
+    }
+}
